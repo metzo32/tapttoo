a9849c9d814ab3e2d541c2664f2356ea3beda912
METZ <metzo3232@gmail.com>
2024-07-19 14:40:51 +0900

    240719 백업, firebase 키 재발급 완, 날짜계산 수정 완

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,40 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics"; //사용자 활동 추적 및 분석.
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// initializeApp 함수로 초기화된 Firebase 앱 객체를 인자로 받아 해댕 앱에 대한 Authentication 인스턴스 생성
-// 이 인스턴스로 사용자 인증 및 관리 (로그인, 로그아웃, 회원가입, 비밀번호 재설정 등)
-
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-//firebase 설정 객체
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAbEveKP8uFbHk4pFIgjJtFReSV0wDElsw",
+  authDomain: "tapttoo-7b2ad.firebaseapp.com",
+  projectId: "tapttoo-7b2ad",
+  storageBucket: "tapttoo-7b2ad.appspot.com",
+  messagingSenderId: "155539663326",
+  appId: "1:155539663326:web:3500f9c8561841d269d54f",
+  measurementId: "G-PHSPLXVRGV"
 };
 
-console.log(firebaseConfig.apiKey)
-
-//위 객체에 처음에는 process.env.REACT_APP_를 붙이지 않아 트래킹하지 못하는 오류 발생.
-//React에서 환경 변수는 모두 REACT_APP_ 접두사가 필요하기 때문이다.
-
-// Firebase 앱 초기화 또는 기존 앱 가져오기
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
-
-
 
 export { app, auth, firestore, analytics };
