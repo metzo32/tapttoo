ea6037ebd127614463c8c3942bed6323da70e388
METZ <metzo3232@gmail.com>
2024-07-24 12:16:53 +0900

    240724 백업

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -5,14 +5,15 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbEveKP8uFbHk4pFIgjJtFReSV0wDElsw",
-  authDomain: "tapttoo-7b2ad.firebaseapp.com",
-  projectId: "tapttoo-7b2ad",
-  storageBucket: "tapttoo-7b2ad.appspot.com",
-  messagingSenderId: "155539663326",
-  appId: "1:155539663326:web:3500f9c8561841d269d54f",
-  measurementId: "G-PHSPLXVRGV"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
